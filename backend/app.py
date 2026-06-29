90983840aebf7dbcb98ab038be6d70b78af2c807
Abraham-Zambrano <lu.zab.vl@gmail.com>
2026-06-29 01:06:06 -0500

    feat: pantalla  login y navegacion chat

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request,redirect
 from flask_socketio import SocketIO
 
 app= Flask(__name__)
 app.config["SECRET_KEY"]= "chat-secret"
 
 socketio=SocketIO(app)
+
+usuarios= []
+
+
+@app.route("/")
+def login():    
+    return render_tmeplate("login.html")
+
+@app.route("/chat",methods=["POST"])
+def chat():
+    
+    usuario=request.form["usuario"]
+
+    return render_template(
+        "chat.html",
+        usuario=usuario
+    )
 
 @app.route("/")
 def inicio():
